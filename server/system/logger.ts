410a271a8a0e3e23a536648250fcc2128b4f7c32
Thibaut <thibaut.mottet@gmail.com>
2026-03-06 19:53:28 +0100

    fix(logs): remove date

--- server/system/logger.ts
+++ server/system/logger.ts
@@ -1,6 +1,3 @@
 import { consola } from 'consola'
 
-consola.options.formatOptions.date = true
-consola.options.formatOptions.columns = 80
-
 export const createLogger = (tag: string) => consola.withTag(tag)
